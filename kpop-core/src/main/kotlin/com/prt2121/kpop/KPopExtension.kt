e6dfdcb9676f0fdbd9789edd63036004084ef7e7
prat t <banqfirn@gmail.com>
2017-02-11 09:43:07 -0500

    port extension to kt

---
+++ kpop-core/src/main/kotlin/com/prt2121/kpop/KPopExtension.kt
@@ -0,0 +1,9 @@
+package com.prt2121.kpop
+
+/**
+ * Gradle extension that holds properties for KPop
+ */
+open class KPopExtension {
+  open var includePattern: String = ""
+  open var excludePattern: String = ""
+}
